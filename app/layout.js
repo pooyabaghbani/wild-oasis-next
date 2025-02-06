56585f9580f2462d74768b471142e3d4eedccf04
pooyabaghbani <pooyabaghbani@gmail.com>
2025-02-06 23:35:47 +0330

    client and server intractions,storing state in url and context api, rendering server components in client components

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import Header from "@/app/_components/Header";
+import { ReservationProvider } from "./_components/ReservationContext";
 
 import "@/app/_styles/globals.css";
 
@@ -23,7 +24,9 @@ function RootLayout({ children }) {
       >
         <Header />
         <div className="flex-1 px-8 py-12 grid">
-          <main className="max-w-7xl mx-auto w-full">{children}</main>
+          <main className="max-w-7xl mx-auto w-full">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
         </div>
       </body>
     </html>
